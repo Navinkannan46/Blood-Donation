5a543a7a61d94251c892887d44390052e8806ee4
Navinkannan46 <navinkannan46@gmail.com>
2025-05-11 10:34:24 +0530

    first commit

--- controller/userController.js
+++ controller/userController.js
@@ -1,5 +1,7 @@
 const userModel = require("../model/userModel");
 const jwt = require("jsonwebtoken")
+const bcrypt = require('bcrypt')
+
 exports.registerController = async (req, res) => {
     console.log("inside registerController ");
     const { username, email, password } = req.body
@@ -8,14 +10,17 @@ exports.registerController = async (req, res) => {
         if (existingUser) {
             res.status(406).json("Alredy existing user.. please login!!!")
         } else {
+            const hashPassword = await bcrypt.hash(password, 10)
             const newUser = new userModel({
-                username, email, password
+                username, email, password:hashPassword
             })
             await newUser.save()
             res.status(200).json(newUser)
         }
     } catch (err) {
         res.status(401).json(err)
+        console.log(err);
+        
     }
 
 }
@@ -25,14 +30,20 @@ exports.loginController = async (req, res) => {
     try {
         const existingUser = await userModel.findOne({ email })
         if (existingUser) {
+            const decodePassword = await bcrypt.compare(password, existingUser.password)
+            if (!decodePassword) {
+                res.status(406).json("Password Error")
+            }
             const token = await jwt.sign({ userId: existingUser._id }, process.env.JWTPASSAWORD)
             res.status(200).json({ user: existingUser, token })
         } else {
-            res.status(406).json("Email not existing.. please register!!!")
+            res.status(406).json("Email  existing.. please register!!!")
 
         }
     } catch (err) {
         res.status(401).json(err)
+        console.log(err);
+        
     }
 
 }
@@ -41,6 +52,7 @@ exports.getAllUsers = async (req, res) => {
     const name = req.query.name
     try {
         const allUser = await userModel.find({ role: "User", username: { $regex: name, $options: 'i' } })
+        console.log(allUser);
         res.status(200).json(allUser)
 
     } catch (error) {
@@ -48,7 +60,6 @@ exports.getAllUsers = async (req, res) => {
 
     }
 }
-
 exports.deleteUser = async (req, res) => {
     const { id } = req.params
     try {
